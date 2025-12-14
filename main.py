b293e19631ed482659003e1128360c5492b7c5cc
sneakyratsniper <159667237+sneakyratsniper@users.noreply.github.com>
2025-12-14 23:31:44 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import base64
+
 def egcd(a, b):
     if b == 0:
         #ax + 0y = GCD(a,0) => x=1 GCD = a
@@ -38,44 +40,57 @@ def generate_rsa_keypair():
 
 def encrypt(message, public_key):
     e, n = public_key
+    if message > n:
+        raise ValueError("Message is too long!")
     return pow(message, e, n)
 
 def decrypt(cipher, private_key):
     d, n = private_key
     return pow(cipher, d, n)
 
-def str_to_int(text):
-    data = text.encode("utf-8")
+    
+def str_to_int(text, type = "utf-8"):
+    if type == "b64":
+        data = base64.b64decode(text.encode("ascii"))
+    else:
+        data = text.encode("utf-8")
     return int.from_bytes(data, byteorder="big")
 
-def int_to_str(number):
+def int_to_str(number, type = "utf-8"):
     length = (number.bit_length() + 7) // 8
     data = number.to_bytes(length, byteorder="big")
-    return data.decode("utf-8")
+    if type == "b64":
+        return base64.b64encode(data).decode("ascii")
+    else:
+        return data.decode("utf-8")
+
 
 public_key,private_key = generate_rsa_keypair()
 
 print("Public key : ",public_key,"\nPrivate key : ",private_key)
 
-
-
 while True:
-        choice = input("Would you like to \nEncrypt (1)\nDecrypt (2)\n")
+        choice = input("Would you like to \nEncrypt (1)\nDecrypt (2)\nExit (3)\n")
         if choice == '1': #Encrypt
-            message = int(input("Enter your message:\n"))
-            print("\nEncrypted text:\n",encrypt(message, public_key))
+            
+            message = input("Enter your message:\n")
+            message = str_to_int(message)
+            code = int_to_str(encrypt(message, public_key),'b64')
+            print("\nEncrypted message:\n",code)
+            
             input("\nPress ENTER to continue...")
             
         elif choice == '2': #Decrypt
-            code = int(input("Enter your code:\n"))
-            print("\nDecrypted text:", decrypt(code, private_key))
+            
+            code = input("Enter your code:\n").strip('')
+            code = str_to_int(code,'b64')
+            message = int_to_str(decrypt(code, private_key))
+            print("\nDecrypted message:", message)
             
             input("\nPress ENTER to continue...")
             
         elif choice == '3':
             break
-        elif choice == '':
-            pass 
             
         else:
             print("Invalid choice. Please enter 1, 2, or 3.")
